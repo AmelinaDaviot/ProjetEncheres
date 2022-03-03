66f8b3a95c9b2829d9ed93db4c6a4f8741b7ecc5
adaviot2021 <adaviot2021@154SE8-GL485Z1.ad.campus-eni.fr>
2022-03-03 11:42:04 +0100

    UtilisateurManager = ajout modifierCompte + supprimerCompte

--- src/main/java/fr/eni/projetencheres/bll/UtilisateurManager.java
+++ src/main/java/fr/eni/projetencheres/bll/UtilisateurManager.java
@@ -37,17 +37,47 @@ public class UtilisateurManager {
 		return user;
 	}
 
+	/**
+	 * Methode permettant à l'utilisateur de s'inscrire
+	 * @param pseudo
+	 * @param nom
+	 * @param prenom
+	 * @param email
+	 * @param telephone
+	 * @param rue
+	 * @param codePostal
+	 * @param ville
+	 * @param motDePasse
+	 * @param confirmation
+	 * @throws Exception = cas où motDePasse et confirmation ne sont PAS EGAUX
+	 */
 	public void sInscrire (String pseudo, String nom, String prenom, String email, String telephone, String rue,
 			String codePostal, String ville, String motDePasse, String confirmation) throws Exception {
 		if (motDePasse.equals(confirmation)) {
 		Utilisateur user = new Utilisateur(pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse);
 		dao.insert(user);
 		} else {
-			Exception BLLException = new Exception("mots de passes diffÃ©rent");
+			Exception BLLException = new Exception("mots de passe different");
 			throw BLLException;
 			
 		} 
 			
-			}
+	}
+	
+	/**
+	 * Méthode pour modifier le compte de l'utilisateur
+	 * @param user
+	 */
+	public void modifierCompte(Utilisateur user) {
+		dao.update(user);
+	}
+	
+	/**
+	 * Méthode pour supprimer le compte de l'utilisateur
+	 * @param noUtilisateur
+	 */
+	public void supprimerCompte(int noUtilisateur) {
+		dao.delete(noUtilisateur);
+	}
 
 }
