e714168e37bee677cfa201f8aaae1df0a06dfa22
tvoltz2021 <tvoltz2021@154SE8-CJ485Z1.ad.campus-eni.fr>
2022-03-07 11:51:26 +0100

    modif fonctionnelle ?

--- src/main/java/fr/eni/projetencheres/servlets/ModificationCompteServlet.java
+++ src/main/java/fr/eni/projetencheres/servlets/ModificationCompteServlet.java
@@ -45,11 +45,69 @@ public class ModificationCompteServlet extends HttpServlet {
 
 		// Recuperation des donnees enregistrees par l'utilisateur et création de
 		// l'utilisateur pour l'utilisateur manager
+		String pseudo = null;
+		String nom = null;
+		String prenom = null;
+		String email = null;
+		String tel = null;
+		String rue = null;
+		String cpo = null;
+		String ville = null;
+		String newMDP = null;
 
-		Utilisateur user = new Utilisateur((int)request.getSession().getAttribute("noUtilisateur") ,request.getParameter("pseudo"), request.getParameter("nom"),
-				request.getParameter("prenom"), request.getParameter("email"), request.getParameter("tel"),
-				request.getParameter("rue"), request.getParameter("cpo"), request.getParameter("ville"),
-				request.getParameter("new-mdp"));
+		if (request.getParameter("pseudo").trim().isEmpty() || request.getParameter("pseudo") == null) {
+			pseudo = (String) request.getSession().getAttribute("pseudo");
+		} else {
+			pseudo = request.getParameter("pseudo");
+		}
+
+		if (request.getParameter("nom").trim().isEmpty() || request.getParameter("nom") == null) {
+			nom = (String) request.getSession().getAttribute("nom");
+		} else {
+			nom = request.getParameter("nom");
+		}
+
+		if (request.getParameter("prenom").trim().isEmpty() || request.getParameter("prenom") == null) {
+			prenom = (String) request.getSession().getAttribute("prenom");
+		} else {
+			prenom = request.getParameter("prenom");
+		}
+
+		if (request.getParameter("email").trim().isEmpty() || request.getParameter("email") == null) {
+			email = (String) request.getSession().getAttribute("email");
+		} else {
+			email = request.getParameter("email");
+		}
+
+		if (request.getParameter("tel").trim().isEmpty() || request.getParameter("tel") == null) {
+			tel = (String) request.getSession().getAttribute("tel");
+		} else {
+			tel = request.getParameter("tel");
+		}
+
+		if (request.getParameter("rue").trim().isEmpty() || request.getParameter("rue") == null) {
+			rue = (String) request.getSession().getAttribute("rue");
+		} else {
+			rue = request.getParameter("rue");
+		}
+		if (request.getParameter("cpo").trim().isEmpty() || request.getParameter("cpo") == null) {
+			cpo = (String) request.getSession().getAttribute("cpo");
+		} else {
+			cpo = request.getParameter("cpo");
+		}
+		if (request.getParameter("ville").trim().isEmpty() || request.getParameter("ville") == null) {
+			ville = (String) request.getSession().getAttribute("ville");
+		} else {
+			ville = request.getParameter("ville");
+		}
+		if (request.getParameter("newMDP").trim().isEmpty() || request.getParameter("newMDP") == null) {
+			newMDP = (String) request.getSession().getAttribute("newMDP");
+		} else {
+			newMDP = request.getParameter("newMDP");
+		}
+
+		Utilisateur user = new Utilisateur((int) request.getSession().getAttribute("noUtilisateur"), pseudo, nom,
+				prenom, email, tel, rue, cpo, ville, newMDP);
 
 		UtilisateurManager um = UtilisateurManager.getInstance();
 		try {
