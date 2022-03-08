a3302a817626e3d1346facb060cfd71a9fbeacdf
adaviot2021 <adaviot2021@154SE8-GL485Z1.ad.campus-eni.fr>
2022-03-08 15:53:58 +0100

    Poursuite mise en place vente article

--- src/main/java/fr/eni/projetencheres/dal/ArticleDAO.java
+++ src/main/java/fr/eni/projetencheres/dal/ArticleDAO.java
@@ -1,10 +1,12 @@
 package fr.eni.projetencheres.dal;
 
 import fr.eni.projetencheres.bo.Article;
+import fr.eni.projetencheres.bo.Retrait;
 
 public interface ArticleDAO {
 	
-	public Article insert(Article art) throws DALException;
+	public Article insert(Article art, Retrait r) throws DALException;
 	public Article selectById(int noArticle);
+
 
 }
--- src/main/java/fr/eni/projetencheres/dal/ArticleImplJdbcDAO.java
+++ src/main/java/fr/eni/projetencheres/dal/ArticleImplJdbcDAO.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import fr.eni.projetencheres.bo.Article;
+import fr.eni.projetencheres.bo.Retrait;
 
 public class ArticleImplJdbcDAO implements ArticleDAO {
 
@@ -27,7 +28,7 @@ public class ArticleImplJdbcDAO implements ArticleDAO {
 	Connection cnx;
 
 	@Override
-	public Article insert(Article art) throws DALException {
+	public Article insert(Article art, Retrait r) throws DALException {
 		try {
 			PreparedStatement stmt = cnx.prepareStatement(INSERT_ARTICLE, 
 					PreparedStatement.RETURN_GENERATED_KEYS);
@@ -38,11 +39,24 @@ public class ArticleImplJdbcDAO implements ArticleDAO {
 			stmt.setDate(4, Date.valueOf(art.getDateFinEnchere()));
 			stmt.setInt(5, art.getPrixInitial());
 			stmt.setInt(6, art.getPrixVente());
-			stmt.setString(7, String.valueOf(art.getEtatVente()));
-			stmt.setString(8, art.getImage());
+			stmt.setInt(7, art.getNoUtilisateur());
+			stmt.setInt(8, art.getNoCategorie());
+			stmt.setString(9, String.valueOf(art.getEtatVente()));
+			stmt.setString(10, art.getImage());
 
 			stmt.executeUpdate();
-
+			
+			ResultSet rs = stmt.getGeneratedKeys();
+			if (rs.next()) {
+				int noArticle = rs.getInt(1);
+				art.setNoArticle(noArticle);
+				stmt.close();
+				
+				stmt = cnx.prepareStatement(INSERT_RETRAIT);
+				stmt.setString(1, r.getRue());
+				stmt.setString(2, r.getCodePostal());
+				stmt.setString(3, r.getVille());
+			}
 			stmt.close();
 		} catch (SQLException e) {
 			throw new DALException("Erreur de mise en vente : " + e.getMessage());
