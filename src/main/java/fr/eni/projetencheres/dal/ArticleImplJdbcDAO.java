e60000d1549c48cdeee6b161096e7f229735a43a
adaviot2021 <adaviot2021@154SE8-GL485Z1.ad.campus-eni.fr>
2022-03-09 14:49:57 +0100

    Mise en vente operationnelle !! ENFIN

--- src/main/java/fr/eni/projetencheres/dal/ArticleImplJdbcDAO.java
+++ src/main/java/fr/eni/projetencheres/dal/ArticleImplJdbcDAO.java
@@ -8,28 +8,34 @@ import java.sql.SQLException;
 
 import fr.eni.projetencheres.bo.Article;
 import fr.eni.projetencheres.bo.Retrait;
+import fr.eni.projetencheres.utils.ConnectionProvider;
 
 public class ArticleImplJdbcDAO implements ArticleDAO {
 
 	private final static String INSERT_ARTICLE = "INSERT INTO ARTICLES_VENDUS "
 			+ "(nom_article, description, date_debut_enchere, date_fin_enchere, prix_initial, prix_vente, "
 			+ "no_utilisateur, no_categorie, etat_vente, image) "
-			+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, DEFAULT, ?)";
 
 	private final static String INSERT_RETRAIT = "INSERT INTO RETRAITS "
 			+ "(no_article, rue, code_postal, ville) "
 			+ "VALUES (?, ?, ?, ?)";
 
-	private final static String SELECT_ARTICLE = "SELECT ARTICLES_VENDUS.*, CATEGORIES.* "
+	private final static String SELECT_ARTICLE_BY_ID = "SELECT ARTICLES_VENDUS.*, CATEGORIES.* "
 			+ "FROM ARTICLES_VENDUS INNER JOIN CATEGORIES "
 			+ "ON ARTICLES_VENDUS.no_categorie = CATEGORIES.no_categorie " 
 			+ "WHERE ARTICLES_VENDUS.no_article = ?";
 
 	Connection cnx;
 
+	public ArticleImplJdbcDAO() throws SQLException {
+		cnx = ConnectionProvider.getConnection();
+	}
+	
 	@Override
 	public Article insert(Article art, Retrait r) throws DALException {
 		try {
+			System.out.println("DAL : entrée dans l'insertion");
 			PreparedStatement stmt = cnx.prepareStatement(INSERT_ARTICLE, 
 					PreparedStatement.RETURN_GENERATED_KEYS);
 			System.out.println(art.toString());
@@ -39,27 +45,34 @@ public class ArticleImplJdbcDAO implements ArticleDAO {
 			stmt.setDate(4, Date.valueOf(art.getDateFinEnchere()));
 			stmt.setInt(5, art.getPrixInitial());
 			stmt.setInt(6, art.getPrixVente());
-			stmt.setInt(7, art.getNoUtilisateur());
-			stmt.setInt(8, art.getNoCategorie());
-			stmt.setString(9, String.valueOf(art.getEtatVente()));
-			stmt.setString(10, art.getImage());
-
+			stmt.setInt(7, art.getVendeur().getNoUtilisateur());
+			stmt.setInt(8, art.getCategorie().getNoCategorie());
+			if (art.getImage().trim().isEmpty()) {
+				stmt.setString(9, null);
+			} else {
+				stmt.setString(9, art.getImage());
+			}
+			
 			stmt.executeUpdate();
 			
 			ResultSet rs = stmt.getGeneratedKeys();
 			if (rs.next()) {
-				int noArticle = rs.getInt(1);
-				art.setNoArticle(noArticle);
+				art.setNoArticle(rs.getInt(1));
+//				int noArticle = rs.getInt(1);
+//				art.setNoArticle(noArticle);
 				stmt.close();
 				
 				stmt = cnx.prepareStatement(INSERT_RETRAIT);
-				stmt.setString(1, r.getRue());
-				stmt.setString(2, r.getCodePostal());
-				stmt.setString(3, r.getVille());
+				stmt.setInt(1, art.getNoArticle());
+				stmt.setString(2, r.getRue());
+				stmt.setString(3, r.getCodePostal());
+				stmt.setString(4, r.getVille());
+				stmt.executeUpdate();
+				stmt.close();
+				System.out.println("fermeture statement retrait");
 			}
-			stmt.close();
+							
 		} catch (SQLException e) {
-			System.out.println("Ca marche pas : " + e.getMessage());
 			throw new DALException("Erreur de mise en vente : " + e.getMessage());
 		}
 		return art;
